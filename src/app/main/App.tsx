68aa4750ad3e57e42ceb93669d5c11a90bd4df74
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2025-05-29 18:32:57 +0300

    feat: update main page

--- src/app/main/App.tsx
+++ src/app/main/App.tsx
@@ -1,6 +1,8 @@
 import { Header } from "../../components/header";
 import styles from "./styles.module.scss";
 import { Footer } from "../../components/footer";
+import logo from "../../../public/logo.jpg";
+import vk from "../../../public/vk.png";
 function App() {
   return (
     <div className={styles.container}>
@@ -9,13 +11,13 @@ function App() {
         <h1 className={styles.heading}>Тестовое задание вк</h1>
         <p className={styles.p}>
           <span>Используемый стек: </span>
-          react, type script,shad cn, redux toolkit, axios, zod
+          react, type script,shad cn, redux toolkit, axios, zod, react-hook-form
         </p>
-        <img className={styles.image} src="./logo.jpg" alt="" />
+        <img className={styles.image} src={logo} alt="" />
         <img
           style={{ animationDelay: "1s" }}
           className={styles.image}
-          src="./vk.png"
+          src={vk}
           alt=""
         />
       </div>
